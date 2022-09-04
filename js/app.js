d1bf19c26defaf36b9ec3be16db77e28e02de826
Sabid Hasan <sabid.soft@gmail.com>
2022-09-04 09:56:47 +0600

    default news added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
+// default news
+fetch('https://openapi.programming-hero.com/api/news/category/08')
+    .then(res => res.json())
+    .then(data => showNews(data.data))
+    .catch(err => alert(err.message))
+
 // fetch categories
 fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(res => res.json())
     .then(data => getCategories(data.data))
-    .catch(err => console.log(err))
+    .catch(err => alert(err.message))
 
 // get categories
 const getCategories = data => {
@@ -36,7 +42,7 @@ const eventHandler = (category_id, category_name) => {
                 spinnerDiv.style.display = 'none'
                 getCategoryInfo(data.data, category_name)
             })
-            .catch(err => console.log(err))
+            .catch(err => alert(err.message))
     })
 }
 
@@ -51,7 +57,7 @@ const getCategoryInfo = (data, category_name) => {
             fetch(`https://openapi.programming-hero.com/api/news/${newsId._id}`)
                 .then(res => res.json())
                 .then(data => showNews(data.data))
-                .catch(err => console.log(err))
+                .catch(err => alert(err.message))
         })
 }
 
@@ -59,6 +65,7 @@ const getCategoryInfo = (data, category_name) => {
 const showNews = newes => {
     const itemsContainer = document.getElementById('items')
     newes.forEach(news => {
+        // console.log(news)
         const {author, thumbnail_url, rating, title, details, total_view, image_url} = news
         const item = document.createElement('div')
         item.classList.add('news-items')
@@ -110,11 +117,41 @@ const showModal = (author, published_date, title, total_view, rating, badge, ima
     `
 }
 
-// spinner function
-const spinner = (isLoading) => {
-    if(isLoading){
-        
-    }else{
-        
-    }
-}
+// // show news
+// const showNews = newes => {
+//     const itemsContainer = document.getElementById('items')
+//     newes.forEach(news => {
+//         const {author, thumbnail_url, rating, title, details, total_view, image_url} = news
+//         const item = document.createElement('div')
+//         item.classList.add('news-items')
+//         item.innerHTML = `
+//             <img class="news-thumbnail" src="${thumbnail_url}" alt="thumbnail of news">
+//             <div class="news-article">
+//                 <div>
+//                     <h4 class="fw-bold fs-4 mb-3">${title}</h4>
+//                     <p class="text-secondary">${details.length > 550 ? details.slice(0, 550) + '...' : details}</p>
+//                 </div>
+//                 <div class="news-item-footer">
+//                     <div class="author-info">
+//                         <img class="image" src="${author.img}" alt="profile picture of author">
+//                         <div class="text">
+//                             <p class="name">${author.name === '' || author.name === null ? 'Name not available' : author.name}</p>
+//                             <p class="date">${new Date(author.published_date).toDateString()}</p>
+//                         </div>
+//                     </div>
+//                     <div class="view-container">
+//                         <i class="fa-regular fa-eye t"></i>
+//                         <p class="views">${total_view === null ? 'Views ot found' : total_view}</p>
+//                     </div>
+//                     <div class="show-more">
+//                         <!-- Button trigger modal -->
+//                         <button onclick="showModal('${author.name}', '${author.published_date}', '${title}', '${total_view}', '${rating.number}', '${rating.badge}', '${image_url}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+//                             Show Details
+//                         </button>
+//                     </div>
+//                 </div>
+//             </div>
+//         `
+//         itemsContainer.appendChild(item)
+//     })
+// }
